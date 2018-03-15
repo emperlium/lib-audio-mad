528e1971497cbf18cfb70714ade6f3d2bb991ea9
Nick Peskett <github.perl@peskett.co.uk>
2018-03-15 10:59:16 +0000

    Added Nick::Audio::MAD docs.

--- README.md
+++ README.md
@@ -15,3 +15,27 @@ On Ubuntu distributions;
     perl Makefile.PL
     make test
     sudo make install
+
+## Example
+
+    use Nick::Audio::MAD;
+    use Nick::MP3::File '$MP3_FRAME';
+
+    my $mp3 = Nick::MP3::File -> new( 'test.mp3' );
+
+    my( $buff_in, $buff_out );
+    my $mad = Nick::Audio::MAD -> new(
+        'buffer_in'     => \$buff_in,
+        'buffer_out'    => \$buff_out,
+        'channels'      => $mp3 -> is_stereo() ? 2 : 1,
+        'gain'          => -3,
+        'debug'         => 1
+    );
+
+    while ( $mp3 -> read_frame() ) {
+        $buff_in .= $MP3_FRAME;
+        $mad -> decode()
+            and printf "decoded %d bytes\n", length( $buff_out );
+    }
+    $mad -> decode( 1 )
+        and printf "decoded %d final bytes\n", length( $buff_out );
--- lib/Nick/Audio/MAD.pm
+++ lib/Nick/Audio/MAD.pm
@@ -15,6 +15,82 @@ BEGIN {
     XSLoader::load 'Nick::Audio::MAD' => $VERSION;
 }
 
+=pod
+
+=head1 NAME
+
+Nick::Audio::MAD - Interface to the libmad library.
+
+=head1 SYNOPSIS
+
+    use Nick::Audio::MAD;
+    use Nick::MP3::File '$MP3_FRAME';
+
+    my $mp3 = Nick::MP3::File -> new( 'test.mp3' );
+
+    my( $buff_in, $buff_out );
+    my $mad = Nick::Audio::MAD -> new(
+        'buffer_in'     => \$buff_in,
+        'buffer_out'    => \$buff_out,
+        'channels'      => $mp3 -> is_stereo() ? 2 : 1,
+        'gain'          => -3,
+        'debug'         => 1
+    );
+
+    while ( $mp3 -> read_frame() ) {
+        $buff_in .= $MP3_FRAME;
+        $mad -> decode()
+            and printf "decoded %d bytes\n", length( $buff_out );
+    }
+    $mad -> decode( 1 )
+        and printf "decoded %d final bytes\n", length( $buff_out );
+
+=head1 METHODS
+
+=head2 new()
+
+Instantiates a new Nick::Audio::MAD object.
+
+All arguments are optional.
+
+=over 2
+
+=item buffer_in
+
+Scalar that'll be used to pull MP3 frames from.
+
+=item buffer_out
+
+Scalar that'll be used to push decoded PCM to.
+
+=item channels
+
+How many audio channels the stream has.
+
+=item gain
+
+Decibels of gain to apply to the decoded PCM.
+
+=item debug
+
+Whether verbose decoding info will be written to STDERR.
+
+=back
+
+=head2 decode()
+
+Decodes the frame (if present) in the buffer_in scalar, returning number of bytes of PCM written to buffer_out.
+
+If the argument is 1, any buffers will be flushed.
+
+    $buff_in .= $MP3_FRAME;
+    $read = $mad -> decode()
+        or next;
+    printf "decoded %d bytes\n", $read;
+    # do something with buffer_out
+
+=cut
+
 sub new {
     my( $class, %settings ) = @_;
     for ( qw( in out ) ) {
